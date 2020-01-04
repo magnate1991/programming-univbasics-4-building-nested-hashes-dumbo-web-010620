c9e7b89313fbcc6a31428da463a58a460352285e
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-04 08:54:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,4 +61,26 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   monopoly = {
+	  :GM => { 
+	    "Malibu" => {
+	      :color => "red"
+	      :speed => "210"
+	    },
+	    :airplane => {
+	      "Havo yullari" => {
+	        :color => "oq",
+	        :speed => '909'
+	      } ,
+	      :Energy => {
+	        "Neftigaz" => {
+	          :color => "kok",
+	          :speed => "011"
+	      }, 
+	      :artel => {
+	        :color => "Joha",
+	        :speed => "300"
+	          }
+	      }
+	}
 end
