1b4cfa7430bc3924028cb76acba7e5db6ccd4ff4
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-04 09:12:33 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,57 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	monopoly = {
+	  :GM => { 
+	    "Malibu" => {
+	      :color => "red"
+	      :speed => "210"
+	    }
+	  }  
+	}
 end
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  # complexity. 
+  monopoly = {
+	  :GM => { 
+	    "Malibu" => {
+	      :color => "red"
+	      :speed => "210"
+	    ,
+	      "Havo yullari" => {
+	        :color => "oq",
+	        :speed => '909'
+	      }
+	    }
+	  }
+	}
 end
 
 def monopoly_with_third_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  # complexity.  
+  monopoly = {
+	  :GM => { 
+	    "Malibu" => {
+	      :color => "red"
+	      :speed => "210"
+	    },
+	      "Havo yullari" => {
+	        :color => "oq",
+	        :speed => '909'
+	      } ,
+	        "Neftigaz" => {
+	          :color => "kok",
+	          :speed => "011"
+	      }
+	  }
+	  } 
 end
 
 def monopoly_with_fourth_tier
@@ -23,4 +59,27 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+   monopoly = {
+	  :GM => { 
+	    "Malibu" => {
+	      :color => "red"
+	      :speed => "210"
+	       }
+	    },
+	      "Havo yullari" => {
+	        :color => "oq",
+	        :speed => '909'
+	        } 
+	      ,
+	        "Neftigaz" => {
+	          :color => "kok",
+	          :speed => "011"
+	        }
+	      , 
+	          "Mir" => {
+	        :color => "Joha",
+	        :speed => "300"
+	          }
+	      }
+	}
 end
